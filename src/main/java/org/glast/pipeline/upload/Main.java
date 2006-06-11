5353f1a4905cabb458811de302cd187a19c2be36
Tony Johnson <11215747+tony-johnson@users.noreply.github.com>
2006-06-11 16:50:22 +0000

    Tidy up

--- src/main/java/org/glast/pipeline/upload/Main.java
+++ src/main/java/org/glast/pipeline/upload/Main.java
@@ -9,7 +9,7 @@ import java.sql.DriverManager;
 /**
  *
  * @author tonyj
- * @version $Id: Main.java,v 1.1 2005-11-14 18:23:46 tonyj Exp $
+ * @version $Id: Main.java,v 1.1.2.1 2006-06-11 16:50:22 tonyj Exp $
  */
 public class Main
 {
@@ -20,13 +20,13 @@ public class Main
    {
       String userName = System.getProperty("user.name");
       
-      Reader in = new FileReader(new File("c:\\tonytest.xml"));
+      Reader in = new FileReader(new File(args[0]));
       
       Class.forName("oracle.jdbc.driver.OracleDriver");
       
-      String url = "jdbc:oracle:thin:@slac-oracle02.slac.stanford.edu:1521:SLACDEV";
-      String username = "GLAST_DP_TEST";
-      String password = "BT33%Q9]MU";
+      String url = System.getProperty("db.url","jdbc:oracle:thin:@glast-oracle01.slac.stanford.edu:1521:GLASTP");
+      String username = System.getProperty("db.user","GLAST_DP");
+      String password = System.getProperty("db.password","BT33%Q9]MU");
       
       Connection connection = DriverManager.getConnection(url,username,password);
       connection.setAutoCommit(false);

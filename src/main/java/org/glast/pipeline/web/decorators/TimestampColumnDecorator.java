e308f29674577ef95cbb9292b08f7df89955146f
Tony Johnson <11215747+tony-johnson@users.noreply.github.com>
2006-06-14 00:27:17 +0000

    Update to displaytag 1.1 Improve log viewer

---
+++ src/main/java/org/glast/pipeline/web/decorators/TimestampColumnDecorator.java
@@ -0,0 +1,52 @@
+package org.glast.pipeline.web.decorators;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Locale;
+import javax.servlet.jsp.PageContext;
+import org.displaytag.decorator.DisplaytagColumnDecorator;
+import org.displaytag.properties.MediaTypeEnum;
+/**
+ *
+ * @author tonyj
+ */
+public class TimestampColumnDecorator implements DisplaytagColumnDecorator, Comparator
+{
+   private Format format;
+   public TimestampColumnDecorator()
+   {
+      this("dd-MMM-yyyy HH:mm:ss.SSS",Locale.US);
+   }
+   public TimestampColumnDecorator(String pattern, Locale locale)
+   {
+      format = new SimpleDateFormat(pattern,locale);
+   }
+   
+   public Object decorate(Object columnValue, PageContext pageContext, MediaTypeEnum media)
+   {
+      try
+      {
+         if (columnValue instanceof oracle.sql.TIMESTAMP)
+         {
+            columnValue = ((oracle.sql.TIMESTAMP) columnValue).timestampValue();
+         }
+      }
+      catch (SQLException x) { x.printStackTrace(); } 
+      System.out.println("class="+columnValue.getClass());
+      return format.format(columnValue);
+   }
+
+   public int compare(Object o1, Object o2)
+   {
+      try
+      {
+         Timestamp ts1 = ((oracle.sql.TIMESTAMP) o1).timestampValue();
+         Timestamp ts2 = ((oracle.sql.TIMESTAMP) o2).timestampValue();
+         return ts1.compareTo(ts2);
+      }
+      catch (SQLException x) { return 0; } 
+   }
+}

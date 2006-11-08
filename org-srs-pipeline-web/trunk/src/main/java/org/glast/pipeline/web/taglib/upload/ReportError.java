b674065cdecfad7b25848ec35bc223f7d4007f83
tonyj <tonyj@106b4de9-e96a-4e09-a5f9-d065fb1fdc55>
2006-11-08 18:34:10 +0000

    New smaller icons (sorry, animation gone for now) Support for deleting task via web interface. Improved handling of errors

--- org-srs-pipeline-web/trunk/src/main/java/org/glast/pipeline/web/taglib/upload/ReportError.java
+++ org-srs-pipeline-web/trunk/src/main/java/org/glast/pipeline/web/taglib/upload/ReportError.java
@@ -1,7 +1,9 @@
 package org.glast.pipeline.web.taglib.upload;
 
+import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Writer;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
@@ -12,13 +14,14 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 public class ReportError extends SimpleTagSupport
 {
    private Throwable error;
+   private boolean brief;
    
    public void doTag() throws JspException, IOException
    {
       JspWriter writer = getJspContext().getOut();
       writer.print("<pre>");
       if (error instanceof JspException && (( JspException) error).getRootCause() != null) error = ((JspException) error).getRootCause();
-      error.printStackTrace(new PrintWriter(writer,true));
+      error.printStackTrace(new PrintWriter(new BriefWriter(writer,brief)));
       writer.print("</pre>");
    }
    
@@ -26,5 +29,46 @@ public class ReportError extends SimpleTagSupport
    {
       this.error = error;
    }
-   
+   public void setBrief(boolean brief)
+   {
+      this.brief = brief;
+   }
+   private static class BriefWriter extends FilterWriter
+   {
+      private boolean filter;
+      private boolean newLine = false;
+      private boolean suppress = false;
+      
+      BriefWriter(Writer writer, boolean filter)
+      {
+         super(writer);
+         this.filter = filter;
+      }
+      
+      public void write(int c) throws IOException
+      {
+         if (c == '\n')
+         {
+            newLine = true;
+         }
+         else if (newLine)
+         {
+            if (c == '\t') suppress = true;
+            else suppress = false;
+            newLine = false;
+         }
+         if (!suppress || !filter) super.write(c);
+      }
+
+      public void write(char[] cbuf, int off, int len) throws IOException
+      {
+         for (int i=0; i<len; i++) write(cbuf[i+off]);
+      }
+
+      public void write(String str, int off, int len) throws IOException
+      {
+         for (int i=0; i<len; i++) write(str.charAt(i+off));
+      }
+      
+   }
 }

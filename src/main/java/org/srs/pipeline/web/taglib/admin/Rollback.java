0f9d116a3819b08b909af1e2d902b28e3567e9e6
Brian Van Klaveren <brianv0@users.noreply.github.com>
2012-12-06 19:44:25 +0000

    Updated dependency to new version of srs web base. Added logging in Rollback

--- src/main/java/org/srs/pipeline/web/taglib/admin/Rollback.java
+++ src/main/java/org/srs/pipeline/web/taglib/admin/Rollback.java
@@ -1,9 +1,12 @@
 package org.srs.pipeline.web.taglib.admin;
 
 import java.sql.Connection;
+import java.util.logging.Logger;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
+import org.srs.groupmanager.GroupManagerWeb;
+import org.srs.groupmanager.UserInfo;
 import org.srs.pipeline.client.PipelineClient;
 import org.srs.pipeline.web.util.ConnectionManager;
 /**
@@ -22,8 +25,13 @@ public class Rollback extends SimpleTagSupport
          Connection conn = ConnectionManager.getConnection((PageContext) getJspContext(), null);
          try
          {
+            UserInfo info = GroupManagerWeb.getUserInfo((PageContext) getJspContext());
+            long t1 = System.currentTimeMillis();
+            Logger.getLogger(Rollback.class.getName()).info(info.getSlacId() + " requests rollback of streams: " + streams.toString() + " and processes: " + processes.toString() + " with args: " + args);
             PipelineClient client = new PipelineClient(conn);
             client.rollback(streams.toString(), processes.toString(), args);
+            long secs = (System.currentTimeMillis() - t1) / 1000;
+            Logger.getLogger(Rollback.class.getName()).info("Rollback finished and took " + secs + " seconds.");
          }
          finally
          {
@@ -32,6 +40,7 @@ public class Rollback extends SimpleTagSupport
       }
       catch (Exception x)
       {
+         Logger.getLogger(Rollback.class.getName()).info("Rollback failed.");
          throw new JspException("Rollback failed",x);
       }
    }
